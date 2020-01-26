2d967af3c1cf58af71cbafbb2d1a46656f7a7cb7
Adam Alvis <adamalvis@Adams-MacBook-Pro.local>
2020-01-26 15:44:56 -0500

    added initial model migrations

---
+++ database/migrations/2020_01_26_203452_create_foods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('calories');
+            $table->integer('protein');
+            $table->integer('fat');
+            $table->integer('carbohydrates');
+            $table->integer('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
